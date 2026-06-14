e398772ded5c13305873b4595517381da1e5b2ae
Isle4525 <aitkazyislam42@gmail.com>
2026-06-14 21:31:09 +0500

    Fix: Move collectstatic to web command and add verbose logging

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn config.wsgi --bind 0.0.0.0:$PORT
-release: python manage.py migrate --noinput && python manage.py collectstatic --noinput
+web: python manage.py collectstatic --noinput && gunicorn config.wsgi --bind 0.0.0.0:$PORT --log-level debug
+release: python manage.py migrate --noinput
--- config/settings.py
+++ config/settings.py
@@ -157,3 +157,31 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    },
+}
